077902939991d12363624635533dbdaeb57112c1
Shawwal <Shawwal2.Shahid@live.uwe.ac.uk>
2023-04-11 03:53:29 +0100

    0.92 Started AI Planning

---
+++ Assets/Scripts/Enemy/AI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+    // Create a circle where if the player steps into the circle The cube will go to the player, Use the Unity pathfinding 
+    // If the square reaches the player then attack based on a parameter
+    // If the player runs away then the enemy returns to its location
+    
+    // Later try to develop my own pathfinding if I have time
+    
+    // If the Cube is hit 3 times then it breaks into 2 smaller cubes then again with 2 hits then 1 hits
+    
+    // Add a Lock on for the camera
+    // Add multiple cubes that communicate with one another so they do not all attack the player.
+    
+    // Have the player Be able to be killed and respawn at a location
+    // Add visual effects for clear emphasis
+}
